e2fc1046b47ee2ef02eeebed3aa6352c7dbe721b
cenk civici <cenk.civici@gmail.com>
2012-04-14 14:53:00 +0300

    IN PROGRESS - issue SKWORKSHOP-18: As an operator I want to add/change products so that the site can sell these products http://redk.it/browse/SKWORKSHOP-18
    Cenk - Display products in index page.

--- Frontend/src/main/com/sk/frontend/web/controller/IndexController.java
+++ Frontend/src/main/com/sk/frontend/web/controller/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sk.service.CategoryService;
+import com.sk.service.ProductService;
 
 @Controller
 @RequestMapping("/index")
@@ -19,10 +20,14 @@ public class IndexController {
 	@Autowired
 	private CategoryService categoryService;
 	
+	@Autowired
+	private  ProductService productService;
+	
 	public IndexController() {}
 	
-	public IndexController(CategoryService categoryService) {
+	public IndexController(CategoryService categoryService,ProductService productService) {
 		this.categoryService = categoryService;
+		this.productService = productService;
 	}
 
 
@@ -31,6 +36,7 @@ public class IndexController {
 	public ModelAndView index(HttpServletRequest request,HttpServletResponse response)  {
 		ModelAndView mav = new ModelAndView("index");
 		mav.addObject("categories", categoryService.getAll());
+		mav.addObject("featuredProducts",productService.getFeaturedProducts());
 		return mav;
 	}
 
--- Frontend/src/test/com/sk/frontend/web/controller/IndexControllerTest.java
+++ Frontend/src/test/com/sk/frontend/web/controller/IndexControllerTest.java
@@ -15,32 +15,46 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sk.domain.Category;
+import com.sk.domain.Product;
 import com.sk.service.CategoryService;
-
+import com.sk.service.ProductService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IndexControllerTest {
 
 	@Mock
 	private CategoryService categoryService;
-	
+
+	@Mock
+	private ProductService productService;
+
 	private IndexController controller;
-	
+
 	@Before
 	public void before() {
-		controller = new IndexController(categoryService);
+		controller = new IndexController(categoryService, productService);
 	}
-	
-	
+
 	@Test
 	public void shouldPopulateModelWithCategories() {
 		List<Category> categories = new ArrayList<Category>();
-		
+
 		when(categoryService.getAll()).thenReturn(categories);
-		
+
 		ModelAndView mav = controller.index(null, null);
-		
-		assertThat(mav.getModel(),hasEntry("categories", (Object) categories));
+
+		assertThat(mav.getModel(), hasEntry("categories", (Object) categories));
+	}
+
+	@Test
+	public void shouldPopulateModelWithFeaturedProducts() {
+		List<Product> products = new ArrayList<Product>();
+
+		when(productService.getFeaturedProducts()).thenReturn(products);
+
+		ModelAndView mav = controller.index(null, null);
+
+		assertThat(mav.getModel(), hasEntry("featuredProducts", (Object) products));
 	}
 
 }
