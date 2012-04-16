dbcfe3596aeae098f9a31bfffdb039283341bb14
cenk civici <cenk.civici@gmail.com>
2012-04-16 12:09:14 +0300

    IN PROGRESS - issue SKWORKSHOP-22: As a shopper, I want to add products to my shopping cartso that I can purchase all at the end of my visit http://redk.it/browse/SKWORKSHOP-22
    ShoppingCartBuilder

--- Common/src/test/com/sk/util/builder/ShoppingCartBuilder.java
+++ Common/src/test/com/sk/util/builder/ShoppingCartBuilder.java
@@ -1,13 +1,25 @@
 package com.sk.util.builder;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.sk.domain.ProductWithQuantity;
 import com.sk.domain.ShoppingCart;
 
 public class ShoppingCartBuilder extends BaseBuilder<ShoppingCart, ShoppingCartBuilder> {
+	
+	private Set<ProductWithQuantity> items = new HashSet<ProductWithQuantity>();
+
 
 	@Override
 	protected ShoppingCart doBuild() {
 		ShoppingCart shoppingCart = new ShoppingCart();
 		return shoppingCart;
+	}
+	
+	public ShoppingCartBuilder(ProductWithQuantity... items) {
+		this.items.addAll(Arrays.asList(items));
 	}
 
 }
