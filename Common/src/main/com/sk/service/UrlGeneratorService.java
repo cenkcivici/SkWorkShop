03272ff6c3160b605e38df520b3bf4ec814975e5
Utku Sonmez <utkusonmez@yahoo.com>
2012-05-07 15:31:26 +0300

    UrlGeneratorService url encode with Turkish characters problem fixed.

--- Common/src/main/com/sk/service/UrlGeneratorService.java
+++ Common/src/main/com/sk/service/UrlGeneratorService.java
@@ -1,32 +1,30 @@
 package com.sk.service;
 
-import java.io.UnsupportedEncodingException;
+import java.text.Normalizer;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.util.UriUtils;
-
-import com.sk.service.exception.ServiceException;
 
 @Service
 public class UrlGeneratorService {
 
 	public String generateUrlFrom(String text) {
 		String url = text.toLowerCase(Locale.ENGLISH);
-		
+
 		url = StringUtils.replace(url, " ", "-");
-		try {
-			url = UriUtils.encodeUri(url, "UTF8");
-		} catch (UnsupportedEncodingException e) {
-			throw new ServiceException(e);
-		}
-		
+		url = unAccentString(url);		
+		url = url.replaceAll("[^a-z 0-9 \\-]*", "");
 		return url;
 	}
 	
-	
-	
-	
+	private String unAccentString(String url){
+		url = url.replaceAll("[ı]", "i");
+		url = Normalizer.normalize(url, Normalizer.Form.NFD);
+		Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+		url = pattern.matcher(url).replaceAll("");
+		return url;
+	}
 
 }
