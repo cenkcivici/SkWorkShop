ef66a4a1e4d2f414189ce54ea34752f5626888db
yigit darcin <yigitdarcin@prime.com.tr>
2012-05-09 20:32:03 +0300

    fix build

--- Admin/src/test/com/sk/admin/web/view/OrdersViewTest.java
+++ Admin/src/test/com/sk/admin/web/view/OrdersViewTest.java
@@ -1,10 +1,5 @@
 package com.sk.admin.web.view;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +15,17 @@ import org.mockito.runners.MockitoJUnitRunner;
 import com.sk.domain.Order;
 import com.sk.domain.OrderStatus;
 import com.sk.service.OrderService;
+import com.sk.service.exception.ServiceException;
 import com.sk.util.builder.OrderBuilder;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static org.mockito.Matchers.anyString;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OrdersViewTest {
@@ -37,16 +42,47 @@ public class OrdersViewTest {
 	}
 	
 	@Test
-	public void shouldRejecteOrder(){
+	public void shouldRejectOrder(){
 		Order order = new OrderBuilder().orderStatus(OrderStatus.UNDERREVIEW).build();
-		
+
 		ordersView.reject(order);
 
-		ArgumentCaptor<Order> captor = ArgumentCaptor.forClass(Order.class);
-		verify(orderService).save(captor.capture());
-		assertThat(captor.getValue().getOrderStatus(), equalTo(OrderStatus.REJECTED));
-		assertThat(order.getOrderStatus(), equalTo(OrderStatus.REJECTED));
+		verify(orderService).rejectOrder(order);
+		verify(facesHelper).addInfoMessage(anyString());
 	}
+	
+	@Test
+	public void shouldRejectOrderAddsErrorMessageIfThereIsException(){
+		Order order = new OrderBuilder().orderStatus(OrderStatus.UNDERREVIEW).build();
+		doThrow(ServiceException.class).when(orderService).rejectOrder(order);
+		ordersView.reject(order);
+		
+		verify(orderService).rejectOrder(order);
+		verify(facesHelper).addErrorMessage(anyString());
+	}
+	
+	
+	@Test
+	public void shouldApproveRefundRequest(){
+		Order order = new OrderBuilder().orderStatus(OrderStatus.UNDERREVIEW).build();
+		
+		ordersView.approveRefundRequest(order);
+		
+		verify(orderService).approveRefundRequest(order);
+		verify(facesHelper).addInfoMessage(anyString());
+	}
+	
+
+	@Test
+	public void shouldApproveRefundOrderAddsErrorMessageIfThereIsException(){
+		Order order = new OrderBuilder().orderStatus(OrderStatus.UNDERREVIEW).build();
+		doThrow(ServiceException.class).when(orderService).approveRefundRequest(order);
+		ordersView.approveRefundRequest(order);
+		
+		verify(orderService).approveRefundRequest(order);
+		verify(facesHelper).addErrorMessage(anyString());
+	}
+	
 	
 	@Test
 	public void shouldCompleteOrder(){
