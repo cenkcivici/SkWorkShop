bae139ab512524353e5d843d6228eeb10bc50d00
cenk civici <cenk.civici@gmail.com>
2012-06-04 00:02:19 +0300

    IN PROGRESS - issue SKWORKSHOP-24: a technical subtask http://redk.it/browse/SKWORKSHOP-24

--- Common/src/main/com/sk/domain/coupon/CategoryCoupon.java
+++ Common/src/main/com/sk/domain/coupon/CategoryCoupon.java
@@ -3,6 +3,7 @@ package com.sk.domain.coupon;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
 
 import com.sk.domain.Category;
 import com.sk.domain.Order;
@@ -14,6 +15,9 @@ public class CategoryCoupon extends Coupon {
 
 	private static final long serialVersionUID = -4580681695558831088L;
 
+	@Transient
+	private int subtask24;
+	
 	@ManyToOne
 	private Category category;
 
